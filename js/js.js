4f06ba7f82b10638e0a1ba06d04dfb3214251f92
thbts <1205552014@qq.com>
2019-09-28 08:06:39 +0800

    001

--- js/js.js
+++ js/js.js
@@ -6,4 +6,5 @@ document.write('<link rel="stylesheet" type="text/css" href="../css/swiper-3.4.2
 //js
 document.write('<script src="../js/jquery.js"><\/script>');
 document.write('<script src="../js/ajax.js"><\/script>');
+document.write('<script src="../js/data.js"><\/script>');
 document.write('<script src="../js/swiper-3.4.2.jquery.min.js"></script>');
